5edf84366322b595e019de693d9e33dc5d4fd539
ikabanen <74080976+ikabanen@users.noreply.github.com>
2020-11-20 16:32:21 -0800

    Update ffmpeg_screen_recorder.bat

--- ffmpeg_screen_recorder.bat
+++ ffmpeg_screen_recorder.bat
@@ -1,1 +1,1 @@
-ffmpeg -f gdigrab -video_size 1920x1080 -framerate 24 -probesize 42M -i desktop -f dshow -i audio="Microphone Array (Sound Blaster Recon3Di)" -preset ultrafast -pix_fmt yuv420p D:\ffmpeg_output\recording.mp4
+ffmpeg -f gdigrab -video_size 1920x1080 -framerate 24 -probesize 42M -i desktop -f dshow -i audio="NAME OF YOUR MICROPHONE" -preset ultrafast -pix_fmt yuv420p recording.mp4
